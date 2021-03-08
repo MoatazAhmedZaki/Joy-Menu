5158fbf2b77bbbced834df0dc851569444939fc0
MoatazAhmedZaki <moatazcs@gmailcom>
2021-03-08 12:59:59 +0200

    Updates

---
+++ precache-manifest.c1398bad3c2dddb8216670914913541a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910856e5e15a777450ae3378fd6c6734",
+    "url": "/Joy-Menu/index.html"
+  },
+  {
+    "revision": "515abd8b84d2de47c430",
+    "url": "/Joy-Menu/static/css/2.25ff2e5b.chunk.css"
+  },
+  {
+    "revision": "cd104ff132d947d3caa4",
+    "url": "/Joy-Menu/static/css/main.ef3031e8.chunk.css"
+  },
+  {
+    "revision": "515abd8b84d2de47c430",
+    "url": "/Joy-Menu/static/js/2.139723ce.chunk.js"
+  },
+  {
+    "revision": "cd104ff132d947d3caa4",
+    "url": "/Joy-Menu/static/js/main.1dd6143e.chunk.js"
+  },
+  {
+    "revision": "60c65477ca4768a7cb52",
+    "url": "/Joy-Menu/static/js/runtime-main.99dc3767.js"
+  },
+  {
+    "revision": "9b698dc629927901b214a529a36c8b54",
+    "url": "/Joy-Menu/static/media/AboutHeader.9b698dc6.png"
+  },
+  {
+    "revision": "ca12945f731bc7c3e98258e93da8cc4a",
+    "url": "/Joy-Menu/static/media/ContactHeader.ca12945f.png"
+  },
+  {
+    "revision": "5a9914a3c4bec5a6805c6433df14a9e1",
+    "url": "/Joy-Menu/static/media/HomeHeader.5a9914a3.png"
+  },
+  {
+    "revision": "4c3c4cb75141f9cd06d46d417d92ff55",
+    "url": "/Joy-Menu/static/media/JoyMob.4c3c4cb7.png"
+  },
+  {
+    "revision": "bc517d7fe05ff8c9bbfbb247ddfbfc47",
+    "url": "/Joy-Menu/static/media/Logo.bc517d7f.png"
+  },
+  {
+    "revision": "45c8b7c0d28d059e8c4702404ab6cc99",
+    "url": "/Joy-Menu/static/media/desh.45c8b7c0.png"
+  },
+  {
+    "revision": "76c33bfabb12d6ca2aaf05ec9df94200",
+    "url": "/Joy-Menu/static/media/enjoy.76c33bfa.png"
+  },
+  {
+    "revision": "2ca6724279c82b93e50db7daf934657a",
+    "url": "/Joy-Menu/static/media/offer.2ca67242.png"
+  }
+]);
